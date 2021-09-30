865cf5442d9aad122e1a108037b0b004e021600c
Elena1987bt <elena.ackovska24@gmail.com>
2021-09-30 11:41:56 +0200

    Added register and login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Home from './pages/home/Home';
+import Watch from './pages/watch/Watch';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 import './app.scss';
 
 function App() {
@@ -10,6 +13,15 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/watch">
+            <Watch />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
         </Switch>
       </div>
